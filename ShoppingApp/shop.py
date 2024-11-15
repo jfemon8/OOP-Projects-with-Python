0338cab91b532fbb328d01f06d8488284cfb9691
Md Jannatul Ferdhous Emon <106956927+jfemon8@users.noreply.github.com>
2024-11-15 21:12:52 +0600

    Delete ShoppingApp directory

--- ShoppingApp/shop.py
+++
@@ -1,9 +0,0 @@
-from product import Products
-
-class Shop:
-    def __init__(self, name, location):
-        self.name = name
-        self.location = location
-        self.products = Products()
-        
-    
